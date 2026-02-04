783c3b6a6b07f8fa880b80eb95e78948b24ce68a
PeddiPoojitha <poojithapeddi14@gmail.com>
2026-02-04 13:15:24 +0530

    loops_for

---
+++ src/Loops_for/Even.java
@@ -0,0 +1,12 @@
+package Loops_for;
+
+public class Even {
+
+	public static void main(String[] args) {
+		for(int i=2;i<=20;i+=2) {
+			System.out.println(i);
+		}
+		
+	}
+
+}
---
+++ src/Loops_for/Increment.java
@@ -0,0 +1,12 @@
+package Loops_for;
+
+public class Increment {
+
+	public static void main(String[] args) {
+		for(int i=5;i<=50;i+=5) {
+			System.out.println(i);
+		}
+
+	}
+
+}
